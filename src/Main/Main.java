4dcbd273fc5813ca6c417e27226916179ba9dcbe
tengfei <teng.tf27@gmail.com>
2017-03-20 15:04:48 +1100

    tengfei

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,8 +3,10 @@ package Main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println(“ds”);
+		// teng
+       System.out.println("ds");
+       
+       
 	}
 
 }

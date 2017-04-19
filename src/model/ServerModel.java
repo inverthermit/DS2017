c95e3d0da6281c0f31a2e789729f01ec2f236a07
tengfei <teng.tf27@gmail.com>
2017-04-19 20:32:27 +1000

    merge

--- src/model/ServerModel.java
+++ src/model/ServerModel.java
@@ -8,71 +8,72 @@ import model.command.Exchange;
 import com.google.gson.Gson;
 
 /**
-	 * Created by Tim Luo on 2017/3/27.
-	 */
+ * Created by Tim Luo on 2017/3/27.
+ */
 public class ServerModel {
-	
+
 	public String hostname;
 	public int port;
 	public Socket socket;
 	public ArrayList<ServerModel> serverList = new ArrayList<ServerModel>();
 	public ArrayList<ClientModel> clientList = new ArrayList<ClientModel>();
 	public ArrayList<Resource> resourceList = new ArrayList<Resource>();
-	
+
 	public ServerModel() {
-		
+
 	}
-	
+
 	public ServerModel(String hostname, int port) {
 		this.hostname = hostname;
 		this.port = port;
 	}
-	
+
 	/**
-	 * addDelResource
-	 * Parameters: Resource(including resource template), isAdd(true for add operation, false for delete operation)
-	 * Return: Integer(1:Successfully added;-1:Resource with same primary key found, add failed;2:Successfully deleted;-2:No such resource to delete)
+	 * addDelResource Parameters: Resource(including resource template),
+	 * isAdd(true for add operation, false for delete operation) Return:
+	 * Integer(1:Successfully added;-1:Resource with same primary key found, add
+	 * failed;2:Successfully deleted;-2:No such resource to delete)
 	 */
-	public synchronized int addDelResource(Resource resource, boolean isAdd){ 
-		if(isAdd){
-			int flag=0;
-			for(int i=0;i<this.resourceList.size();i++){
+	public synchronized int addDelResource(Resource resource, boolean isAdd) {
+		if (isAdd) {
+			int flag = 0;
+			for (int i = 0; i < this.resourceList.size(); i++) {
 				Resource element = this.resourceList.get(i);
-				if(resource.owner.equals(element.owner)&&resource.channel.equals(element.channel)&&resource.uri.equals(element.uri)){
-					flag=1;
+				if (resource.owner.equals(element.owner) && resource.channel.equals(element.channel)
+						&& resource.uri.equals(element.uri)) {
+					flag = 1;
 					break;
 				}
 			}
-			if(flag==0){
+			if (flag == 0) {
 				this.resourceList.add(resource);
 				return 1;
-			}
-			else{
+			} else {
 				return -1;
 			}
-		}
-		else{
-			int flag = 0;//Record if there's a successful deletion
-			for(int i=0;i<this.resourceList.size();i++){
+		} else {
+			int flag = 0;// Record if there's a successful deletion
+			for (int i = 0; i < this.resourceList.size(); i++) {
 				Resource element = this.resourceList.get(i);
-				if(resource.owner.equals(element.owner)&&resource.channel.equals(element.channel)&&resource.uri.equals(element.uri)){
+				if (resource.owner.equals(element.owner) && resource.channel.equals(element.channel)
+						&& resource.uri.equals(element.uri)) {
 					this.resourceList.remove(i);
-					flag=1;
+					flag = 1;
 				}
 			}
-			if(flag==1){
+			if (flag == 1) {
 				return 2;
-			}
-			else{
+			} else {
 				return -2;
 			}
 		}
 	}
-	public String toServerListJson(){
+
+	public String toServerListJson() {
 		String command = "EXCHANGE";
-		Exchange exchange = new Exchange(command,this.serverList);
+		Exchange exchange = new Exchange(command, this.serverList);
 		String json = exchange.toJSON();
 		return json;
 	}
-	
+
 }
--- src/server/Operation.java
+++ src/server/Operation.java
@@ -1,4 +1,5 @@
 package server;
+
 import model.ClientModel;
 import model.ServerModel;
 import model.Response.NormalResponse;
@@ -16,11 +17,11 @@ import tool.Common;
 import tool.Config;
 
 public class Operation {
-	public ArrayList<String> dispatcher(String json, ServerModel server, ClientModel client){
-		//1.get the command of json
+	public ArrayList<String> dispatcher(String json, ServerModel server, ClientModel client) {
+		// 1.get the command of json
 		String op = Common.getOperationfromJson(json);
 		ArrayList<String> result = null;
-		switch (op){
+		switch (op) {
 		case "PUBLISH":
 			Publish publish = new Publish();
 			publish.fromJSON(json);
@@ -57,76 +58,81 @@ public class Operation {
 		}
 		return result;
 	}
-	
-	public ArrayList<String> doClientExchange(Exchange exchange,ServerModel server){
-		System.out.println("doClientExchange:"+exchange.toJSON());
+
+	public ArrayList<String> doClientExchange(Exchange exchange, ServerModel server) {
+		System.out.println("doClientExchange:" + exchange.toJSON());
 		NormalResponse nr = new NormalResponse("success");
 		ArrayList<String> arr = new ArrayList<String>();
 		arr.add(nr.toJSON());
 		return arr;
 	}
-	
-	//TODO:Add file stream transfer API!!!!!!!!! This is a special operation
-	public ArrayList<String> doClientFetch(Fetch fetch,ServerModel server, ClientModel client){
+
+	// TODO:Add file stream transfer API!!!!!!!!! This is a special operation
+	public ArrayList<String> doClientFetch(Fetch fetch, ServerModel server, ClientModel client) {
 		String fileName = (String) fetch.getResource().uri;
 		// Check if file exists
 		File f = new File(fileName);
-		if(f.exists()){
-			// Send this back to client so that they know what the file is.			
+		if (f.exists()) {
+			// Send this back to client so that they know what the file is.
 			try {
-				DataOutputStream output =new DataOutputStream( client.socket.getOutputStream());
+				DataOutputStream output = new DataOutputStream(client.socket.getOutputStream());
 				// Send trigger to client
 				NormalResponse nr = new NormalResponse("success");
 				output.writeUTF(nr.toJSON());
 				fetch.getResource().resourceSize = f.length();
-				//System.out.println(fetch.getResource().toJSON());
-				output.writeUTF(fetch.getResource().toJSON());				
+				// System.out.println(fetch.getResource().toJSON());
+				output.writeUTF(fetch.getResource().toJSON());
 				// Start sending file
-				RandomAccessFile byteFile = new RandomAccessFile(f,"r");
+				RandomAccessFile byteFile = new RandomAccessFile(f, "r");
 				byte[] sendingBuffer = new byte[Config.TRUNK_SIZE];
 				int num;
 				// While there are still bytes to send..
-				while((num = byteFile.read(sendingBuffer)) > 0){
+				while ((num = byteFile.read(sendingBuffer)) > 0) {
 					output.write(Arrays.copyOf(sendingBuffer, num));
 				}
 				byteFile.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-		else{
+		} else {
 			// Throw an error here..
 		}
-		return null;	
-	}
-	public ArrayList<String> doClientPublish(Publish publish,ServerModel server){
-		server.addDelResource(publish.getResource(), true); 
 		return null;
 	}
-	public ArrayList<String> doClientQuery(Query query,ServerModel server){
+
+	public ArrayList<String> doClientPublish(Publish publish, ServerModel server) {
+		server.addDelResource(publish.getResource(), true);
+		return null;
+	}
+
+	public ArrayList<String> doClientQuery(Query query, ServerModel server) {
 
 		return null;
 	}
-	public ArrayList<String> doClientRemove(Remove remove,ServerModel server){
-		server.addDelResource(remove.getResource(), false);//Add getters and setters
+
+	public ArrayList<String> doClientRemove(Remove remove, ServerModel server) {
+		server.addDelResource(remove.getResource(), false);// Add getters and
+															// setters
 		return null;
 	}
-	public ArrayList<String> doClientShare(Share share,ServerModel server){
-		//TODO:Check if the resource uri is a file which exists
+
+	public ArrayList<String> doClientShare(Share share, ServerModel server) {
+		// TODO:Check if the resource uri is a file which exists
 		server.addDelResource(share.getResource(), true);
 		return null;
 	}
-	
-	//Send using the EXCHANGE request to other servers in server.serverList
-	public ArrayList<String> doServerExchange(ServerModel server){
+
+	// Send using the EXCHANGE request to other servers in server.serverList
+	public ArrayList<String> doServerExchange(ServerModel server) {
 		String query = server.toServerListJson();
-		System.out.println("In doServerExchange"+query);
-		for(int i=0;i<server.serverList.size();i++){
+		System.out.println("In doServerExchange" + query);
+		for (int i = 0; i < server.serverList.size(); i++) {
 			ServerModel tempServer = server.serverList.get(i);
-			System.out.println("In doServerExchange start client.doSend"+tempServer.hostname+tempServer.port);
-			//TODO: java.net.ConnectException Then Delete the address (Add a return to doSend)
+			System.out.println("In doServerExchange start client.doSend" + tempServer.hostname + tempServer.port);
+			// TODO: java.net.ConnectException Then Delete the address (Add a
+			// return to doSend)
 			Client.doSend(tempServer.hostname, tempServer.port, query);
-			System.out.println("Finished doServerExchange start client.doSend"+tempServer.hostname+tempServer.port);
+			System.out.println("Finished doServerExchange start client.doSend" + tempServer.hostname + tempServer.port);
 		}
 		return null;
 	}

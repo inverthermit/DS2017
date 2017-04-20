45a66177e2460e67ef0702b3107b7fe739af8bbe
tengfei <teng.tf27@gmail.com>
2017-04-20 16:40:24 +1000

    0420

--- src/client/TesterClient.java
+++ src/client/TesterClient.java
@@ -9,13 +9,13 @@ import tool.Common;
 import tool.Config;
 public class TesterClient {
 	public static void main(String[] args){
-		int serverPort = 10000;//3780;
-		String serverHostname = "127.0.0.1";//"sunrise.cis.unimelb.edu.au";
+		int serverPort = 3780;//3780;
+		String serverHostname = "sunrise.cis.unimelb.edu.au";//"sunrise.cis.unimelb.edu.au";
 		//TODO: 1. Check if parameters are valid
 		//Commandline.isValid(args);
 		//TODO:2.Translate cli to query
 		//{"EXCHANGE","FETCH","SHARE","PUBLISH","QUERY","REMOVE"};
-		String queryExample = "{\"command\": \"QUERY\",    \"reply\": true, \"resource\": {\"name\": \"\",\"tags\": [],\"description\": \"\",\"uri\": \"\",\"channel\": \"\",\"owner\": \"\",\"ezserver\": null}}";
+		String queryExample = "{\"command\": \"QUERY\",    \"reply\": true, \"resourceTemplate\": {\"name\": \"\",\"tags\": [],\"description\": \"\",\"uri\": \"\",\"channel\": \"\",\"owner\": \"\",\"ezserver\": null}}";
 	    String publishExample = "{ \"command\": \"PUBLISH\", \"resource\" : { \"name\" : \"Unimelb website 6667\", \"tags\" : [\"web\", \"html\"], \"description\" : \"The main page for the University of Melbournee\", \"uri\" : \"http://www.unimelb1.edu.au\", \"channel\" : \"\", \"owner\" : \"\", \"ezserver\" : null } }";
 	    String shareExample = "{ \"command\": \"SHARE\", \"secret\": \"2os41f58vkd9e1q4ua6ov5emlv\", \"resource\": { \"name\": \"EZShare JAR\", \"tags\": [ \"jar\" ], \"description\": \"The jar file for EZShare. Use with caution.\", \"uri\":\"file:\\/\\/\\/\\/home\\/aaron\\/EZShare\\/ezshare.jar\", \"channel\": \"my_private_channel\", \"owner\": \"aaron010\", \"ezserver\": null } }";
 	    String removeExample = "{ \"command\": \"REMOVE\", \"resource\": { \"name\": \"aaa\", \"tags\": [], \"description\": \"\", \"uri\": \"http:\\/\\/www.unimelb1.edu.au\", \"channel\": \"\", \"owner\": \"\", \"ezserver\": null } }";
---
+++ src/tool/ServerCommandLine.java
@@ -0,0 +1,106 @@
+package tool;
+import org.apache.commons.cli.*;
+
+import model.ServerModel;
+/**
+	 * Created by Tim Luo on 2017/3/27.
+	 */
+public class ServerCommandLine {
+	public static void main(String[] args) {
+        Options opt = new Options();
+        // add command line option to options
+        opt.addOption("advertisedhostname", true, "advised hostname");
+        Option connectionintervallimit = Option.builder("connectionintervallimit")
+        		.hasArg()
+        		.desc("connection interval limit in seconds")
+        		.build();
+        opt.addOption(connectionintervallimit);
+        Option exchangeinterval = Option.builder("exchangeinterval")
+        		.hasArg()
+        		.desc("exchange interval in seconds")
+        		.build();
+        opt.addOption(exchangeinterval);
+        Option port = Option.builder("port")
+        		.hasArg()
+        		.desc("server port, an integer")
+        		.build();
+        opt.addOption(port);
+        Option secret = Option.builder("secret")
+        		.hasArg()
+        		.desc("secret")
+        		.build();
+        opt.addOption(secret);
+        Option debug = Option.builder("debug")
+        		.desc("print debug informaton")
+        		.build();
+        opt.addOption(debug);
+        opt.addOption("h", "help", false, "help");
+        
+        String formatterHelp  = "WRONG COMMAND" + "\n" + "HELP";
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLineParser parser = new  DefaultParser();
+        CommandLine commandLine = null;
+        //if command is wrong print out help message
+        try {
+            commandLine = parser.parse( opt, args);
+        
+        // if command line has -h or -help
+        if (commandLine.hasOption("h")) {
+            HelpFormatter hf = new HelpFormatter();
+            hf.printHelp(formatterHelp, "", opt, "");
+            System.out.println("has h ");
+            return;
+        }
+        
+        // if command line has -advertisedhostname 
+        if (commandLine.hasOption("advertisedhostname")) {
+        	String hostName = commandLine.getOptionValue("advertisedhostname");
+        	ServerModel server = new ServerModel();
+        	server.setHostName(hostName);
+            System.out.println("has advertisedhostname and its arg is " + hostName);
+        }
+        
+        // if command line has -connectionintervallimit 
+        if (commandLine.hasOption("connectionintervallimit")) {
+        	String intervalLimit = commandLine.getOptionValue("connectionintervallimit");
+        	ServerModel server = new ServerModel();
+        	server.setIntervalLimit(intervalLimit);
+            System.out.println("has connectionintervallimit and its arg is " + intervalLimit);
+        }
+        
+       // if command line has -exchangeinterval 
+        if (commandLine.hasOption("exchangeinterval")) {
+        	String exchangeInterval = commandLine.getOptionValue("exchangeinterval");
+        	ServerModel server = new ServerModel();
+        	server.setExchangeInterval(exchangeInterval);
+            System.out.println("has exchangeinterval and its arg is " + exchangeinterval);
+        }
+        
+        // if command line has -port
+        if (commandLine.hasOption("port")) {
+        	String portName = commandLine.getOptionValue("port");
+        	int portNum;
+        	portNum = Integer.parseInt(portName);
+        	ServerModel server = new ServerModel();
+        	server.setPort(portNum);
+            System.out.println("has port and its arg is " + portNum);
+        }
+        
+      //if command line has -secret
+        if (commandLine.hasOption("secret")) {
+        	String secretName = commandLine.getOptionValue("secret");
+        	ServerModel server = new ServerModel();
+        	server.setSecret(secretName);
+            System.out.println("has secret and its arg is " + secretName);
+        }
+        
+        // if command line has -debug
+        if (commandLine.hasOption("debug")) {
+            System.out.println("has debug");
+        }
+        } catch (ParseException e) {
+            formatter.printHelp( formatterHelp,opt ); 
+        }
+        }
+	
+}

bc4d04a083975d4af83663272bb4298a54cda3d2
junwenz <junwenz@student.unimelb.edu.au>
2017-05-14 17:21:39 +1000

    update: secure thread

--- src/server/ServerSocketSSLThread.java
+++ src/server/ServerSocketSSLThread.java
@@ -1,7 +1,11 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,6 +36,9 @@ public class ServerSocketSSLThread implements Runnable {
 		// TODO Auto-generated method stub
 		ExecutorService pool = Executors.newCachedThreadPool();
 		pool.execute(new HeartbeatThread(selfModel));
+		System.setProperty("javax.net.ssl.keyStore","serverKeystore/aGreatName");
+		System.setProperty("javax.net.ssl.keyStorePassword","comp90015");
+		System.setProperty("javax.net.debug","all");
 		try {
 			
 			Log.log(Common.getMethodName(), "INFO", "started");
@@ -39,33 +46,41 @@ public class ServerSocketSSLThread implements Runnable {
 			SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory
 					.getDefault();
 			SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(sport);
+			SSLSocket sslSocket = (SSLSocket)sslserversocket.accept();
+			boolean bool =true;
+			while(bool){
+				PrintWriter printwriter=new PrintWriter(sslSocket.getOutputStream(),true);
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+				BufferedReader commandPromptBufferedReader=new BufferedReader(new InputStreamReader(System.in));
+				
+			}
 			
-			boolean bool = true;
+			//boolean bool = true;
 			while (bool) {
 				ClientModel client = new ClientModel();
 				// Log.log(Common.getMethodName(), "INFO", "started");
 				client.sslsocket = (SSLSocket)sslserversocket.accept();
-				client.ip = client.socket.getRemoteSocketAddress().toString().split(":")[0];
+				client.ip = client.sslsocket.getRemoteSocketAddress().toString().split(":")[0];
 				long timestamp = Common.getCurrentSecTimestamp();
 				if (selfModel.ipTimeList == null) {
 					selfModel.ipTimeList = new HashMap<String, Long>();
 				} else if (timestamp - selfModel.getLastClientTime(client.ip) < Config.CONNECTION_LIMIT_INTERVAL) {
 					System.out.println("Less than connection limit interval");
-					client.socket.close();
+					client.sslsocket.close();
 					continue;
 				}
 				selfModel.ipTimeList.put(client.ip, timestamp);
 				// client.timestamp = timestamp;
 
 				// Timeout
-				client.socket.setSoTimeout(Config.CONNECTION_TIMEOUT);
+				client.sslsocket.setSoTimeout(Config.CONNECTION_TIMEOUT);
 				// System.out.println(client.socket.getSoTimeout());
 				selfModel.clientList.add(client);
 				// TODO: Output client connection log
 				// System.out.println("Connected");
 				Log.log(Common.getMethodName(), "INFO",
-						"New Connection:" + client.socket.getRemoteSocketAddress().toString().split(":")[0] + ":"
-								+ client.socket.getPort());
+						"New Connection:" + client.sslsocket.getRemoteSocketAddress().toString().split(":")[0] + ":"
+								+ client.sslsocket.getPort());
 				pool.execute(new ServerThread(client, selfModel));
 			}
 			sslserversocket.close();

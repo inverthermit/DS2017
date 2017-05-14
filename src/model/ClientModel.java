20ff74ce43484649962c95ccf9c42f65436bad02
张子一 <zhangziyi@zhangziyideMacBook-Pro.local>
2017-05-14 16:48:56 +1000

    Update: sslsecuresocket thread

--- src/model/ClientModel.java
+++ src/model/ClientModel.java
@@ -6,6 +6,8 @@ package model;
 
 import java.net.Socket;
 import java.util.ArrayList;
+
+import javax.net.ssl.SSLSocket;
 /**
  * This class is model of clients.
  * It contains ip, port, socket, serverList, resourceList
@@ -18,6 +20,7 @@ public class ClientModel {
 	public String ip;
 	public String port;
 	public Socket socket;
+	public SSLSocket sslsocket;
 	public ArrayList<ServerModel> serverList;
 	public ArrayList<Resource> resourceList;
 	public long timestamp;
--- src/server/ServerSocketSSLThread.java
+++ src/server/ServerSocketSSLThread.java
@@ -1,5 +1,80 @@
 package server;
 
-public class ServerSocketSSLThread {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+import model.ClientModel;
+import model.ServerModel;
+import tool.Common;
+import tool.Config;
+import tool.Log;
+
+public class ServerSocketSSLThread implements Runnable {
+	private int sport;
+	private ServerModel selfModel;
+	
+	
+	
+	public ServerSocketSSLThread(int sport, ServerModel selfModel) {
+		this.sport = sport;
+		this.selfModel = selfModel;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		ExecutorService pool = Executors.newCachedThreadPool();
+		pool.execute(new HeartbeatThread(selfModel));
+		try {
+			Log.log(Common.getMethodName(), "INFO", "bound to port " + sport);
+			Log.log(Common.getMethodName(), "INFO", "started");
+			
+			SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory
+					.getDefault();
+			SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(sport);
+			
+			boolean bool = true;
+			while (bool) {
+				ClientModel client = new ClientModel();
+				// Log.log(Common.getMethodName(), "INFO", "started");
+				client.sslsocket = (SSLSocket)sslserversocket.accept();
+				client.ip = client.socket.getRemoteSocketAddress().toString().split(":")[0];
+				long timestamp = Common.getCurrentSecTimestamp();
+				if (selfModel.ipTimeList == null) {
+					selfModel.ipTimeList = new HashMap<String, Long>();
+				} else if (timestamp - selfModel.getLastClientTime(client.ip) < Config.CONNECTION_LIMIT_INTERVAL) {
+					System.out.println("Less than connection limit interval");
+					client.socket.close();
+					continue;
+				}
+				selfModel.ipTimeList.put(client.ip, timestamp);
+				// client.timestamp = timestamp;
+
+				// Timeout
+				client.socket.setSoTimeout(Config.CONNECTION_TIMEOUT);
+				// System.out.println(client.socket.getSoTimeout());
+				selfModel.clientList.add(client);
+				// TODO: Output client connection log
+				// System.out.println("Connected");
+				Log.log(Common.getMethodName(), "INFO",
+						"New Connection:" + client.socket.getRemoteSocketAddress().toString().split(":")[0] + ":"
+								+ client.socket.getPort());
+				pool.execute(new ServerThread(client, selfModel));
+			}
+			sslserversocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			Log.log(Common.getMethodName(), "INFO", "Server fail to start: Port already in used(" + sport + ")");
+			System.exit(0);
+		}
+	}
+	
 }
